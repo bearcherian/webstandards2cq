a15a950e985002d60d0ead58a25009e51c296409
bcherian <benjamin.cherian@villanova.edu>
2015-01-01 02:14:55 -0600

    Updated input factory to return MultiValueInput for checkboxes and radio

--- src/main/java/edu/villanova/convert/forms/input/InputFactory.java
+++ src/main/java/edu/villanova/convert/forms/input/InputFactory.java
@@ -9,19 +9,20 @@ public class InputFactory {
 
     public static Input getInput(Element element){
         String tagName = element.tagName();
+        String type = element.attr("type").toLowerCase();
         Input input = null;
         if (tagName == null) {
             return null;
         }
 
-        if(tagName.equalsIgnoreCase("input")){
-            input =  new Input();
-        } else if (tagName.equalsIgnoreCase("select")) {
-            input = new SelectInput();
+        if (tagName.equalsIgnoreCase("select") || "checkbox".equals(type) || "radio".equals(type)) {
+            input = new MultiValueInput();
         } else if (tagName.equalsIgnoreCase("textarea")) {
             input = new TextareaInput();
+        } else if(tagName.equalsIgnoreCase("input")){
+            input =  new Input();
         }
 
-        return input;
+            return input;
     }
 }
